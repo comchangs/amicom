803326f94f4034dc9069a4abb0abbbd483af0de1
lgy7229 <lgy7229@e0208d96-c164-4637-a2c2-061d0c46a8bd>
2011-08-08 10:39:31 +0000

    처음

---
+++ jyoung/src/ajou/amicom/Caltest/PopActivity.java
@@ -0,0 +1,15 @@
+package ajou.amicom.Caltest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PopActivity extends Activity {
+
+	/** Called when the activity is first created. */
+
+//	public void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.main);
+
+//}
+}
