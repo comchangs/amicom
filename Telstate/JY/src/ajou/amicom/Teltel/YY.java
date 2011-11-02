ee850ab2a6c204c0782c16214ccad63ab1d8e08b
lgy7229 <lgy7229@e0208d96-c164-4637-a2c2-061d0c46a8bd>
2011-11-02 09:52:55 +0000

    aaa

---
+++ Telstate/JY/src/ajou/amicom/Teltel/YY.java
@@ -0,0 +1,14 @@
+package ajou.amicom.Teltel;
+
+import ajou.amicom.Teltel.R;
+import android.app.*;
+import android.os.*;
+import android.telephony.*;
+
+public class YY extends Activity {
+	TelephonyManager mTelMan;
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main2);
+	}
+}
