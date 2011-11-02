493984c4159d3e006e53b03a1153301a6d066dc1
hee220022 <hee220022@e0208d96-c164-4637-a2c2-061d0c46a8bd>
2011-11-02 10:50:36 +0000

    얄루 내가짱!

---
+++ Telstate/TelStatePsh/src/ajou/amicom/Psh/TelStatePsh2.java
@@ -0,0 +1,14 @@
+package ajou.amicom.Psh;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TelStatePsh2 extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.pictuer2);
+
+	}
+
+
+}
